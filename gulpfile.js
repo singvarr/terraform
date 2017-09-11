d84db0ea9f86d650c8896ef016e090073763006a
sihorjs <sihorjs@gmail.com>
2017-09-11 17:59:13 +0300

    uninstalled unnecessary plugins

--- gulpfile.js
+++ gulpfile.js
@@ -12,36 +12,29 @@ const gulp = require('gulp');
 	inject = require('gulp-inject');
 	series = require('stream-series');
 	htmlMin = require('gulp-htmlmin');
-	clean = require('del');
+	ifElse = require('gulp-if-else');
 
 gulp.task('styles', () => {
-	return gulp.src('./src/less/main.less')
-		.pipe(less({
-			plugins: [autoprefix]
-		}))
-		.pipe(gulp.dest('./dist/css'))
-		.pipe(less({
-			plugins: [autoprefix]
-		}))
-		.pipe(cleanCSS({
-			level: 2
-		}))
+	return gulp.src('src/less/main.less')
+		.pipe(less({plugins: [autoprefix]}))
+		.pipe(cleanCSS({level: 2}))
 		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('./dist/css'))
+		.pipe(gulp.dest('dist/css'))
+		.pipe(gulp.dest('dist/css'))	
 });
 
 gulp.task('scripts', () => {
-	return gulp.src('./src/js/*.js')
+	return gulp.src('src/js/**/*.js')
 		.pipe(concat('bundle.js'))
-		.pipe(gulp.dest('./dist/js'))
+		.pipe(gulp.dest('dist/js'))
 		.pipe(concat('bundle.js'))
 		.pipe(uglify())
 		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('./dist/js'))
+		.pipe(gulp.dest('dist/js'))
 });
 
 gulp.task('images', () => {
-	return gulp.src('./src/assets/**/*.*')
+	return gulp.src('src/assets/**/*.*')
 		.pipe(imagemin([
 			imageminPngquant({
 				floyd: 0.3,
@@ -54,12 +47,12 @@ gulp.task('images', () => {
 				quality: 'veryhigh'
 			})
 		]))
-		.pipe(gulp.dest('./dist/assets'))
+		.pipe(gulp.dest('dist/assets'))
 });
 
 gulp.task('fonts', () => {
-	return gulp.src('./src/fonts/**/*.*')
-		.pipe(gulp.dest('./dist/fonts'))
+	return gulp.src('src/fonts/**/*.*')
+		.pipe(gulp.dest('dist/fonts'))
 });
 
 gulp.task('vendor', () => {
@@ -70,12 +63,12 @@ gulp.task('vendor', () => {
 gulp.task('html', () => {
 	const streams = {
 		vendorJS: gulp.src('dist/vendor/**/*.min.js', {read: false}),
-		appJS: gulp.src('dist/js/**/*.min.js', {read: false}),
 		vendorCSS: gulp.src('dist/vendor/**/*.css', {read: false}),
+		appJS: gulp.src('dist/js/**/*.min.js', {read: false}),
 		appCSS: gulp.src(['dist/css/**/*.min.css', 'dist/fonts/**/*.css'], {read: false})
 	}
 
-	return gulp.src('./src/templates/*.html', {base: './src/templates'})
+	return gulp.src('src/*.html')
 		.pipe(inject(streams.vendorCSS, {
 			name: 'vendorCSS',
 			relative: true
@@ -92,10 +85,10 @@ gulp.task('html', () => {
 		.pipe(gulp.dest('dist'))
 });
 
-gulp.task('default', function () {
-	gulp.watch('src/less/*.less', ['styles']);
-	gulp.watch('src/js/*.js', ['scripts']);
-	gulp.watch('src/*.html', ['html']);
+gulp.task('default', () => {
+	gulp.watch('./src/*.html', ['html']);
+	gulp.watch('src/less/**/*.less', ['styles']);
+	gulp.watch('src/js/**/*.js', ['scripts']);
 })
 
 	
