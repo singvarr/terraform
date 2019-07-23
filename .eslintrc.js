2fd82396e00a06132e0d375d4c14b4e8ac0b14fc
sihorjs <sihorjs@gmail.com>
2019-07-23 19:55:57 +0300

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    extends: ["eslint:recommended"],
+    rules: {
+        camelcase: "error",
+        "max-len": "error",
+        indent: ["error", 4, { SwitchCase: 1 }],
+        quotes: "error",
+        "object-curly-spacing": ["error", "always"],
+        "eol-last": "error",
+        "comma-dangle": "error",
+        "no-trailing-spaces": "error",
+        "default-case": "error",
+        "no-var": "error",
+        semi: "error",
+        "no-useless-computed-key": "error"
+    },
+    env: {
+        es6: true,
+        node: true,
+        browser: true
+    }
+};
