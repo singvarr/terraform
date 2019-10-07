003f1cce047906a6a1414ea55e8f85fff2a8ce5f
sihorjs <sihorjs@gmail.com>
2019-10-07 19:46:00 +0300

    use es6 syntax, fix eslint errors

--- src/js/counters.js
+++ src/js/counters.js
@@ -1,12 +1,15 @@
 const $ = require("jquery");
 
-$(document).ready(function () {
-    
-	$(window).scroll(function () {
-	    if ($('.counters').length && $(window).scrollTop() > $('.counters').offset().top - $(window).height()) {
-            $('#final').html(38);
-            $('#like').html(35);
-            $('#client').html(12);
-	    }
-	});
-})
+$(document).ready(() => {
+    $(window).scroll(() => {
+        if (
+            $(".counters").length &&
+            $(window).scrollTop() >
+                $(".counters").offset().top - $(window).height()
+        ) {
+            $("#final").html(38);
+            $("#like").html(35);
+            $("#client").html(12);
+        }
+    });
+});
