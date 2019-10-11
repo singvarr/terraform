de68f85c5ba20e2dd9eb81674d3a25fd444a6d35
Ihor Sokolov <sihorjs@gmail.com>
2019-10-11 23:31:45 +0300

    8. Create fixtures (#14)
    * add functions and dictionaries to generate fake data
    
    * add root option
    
    * install faker

---
+++ src/fixtures/menu.js
@@ -0,0 +1,34 @@
+const leftMenu = [
+    {
+        translationKey: "navigation.about",
+        href: "#about"
+    },
+    {
+        translationKey: "navigation.services",
+        href: "/services"
+    },
+    {
+        translationKey: "navigation.portfolio",
+        href: "/portfolio"
+    }
+];
+
+const rightMenu = [
+    {
+        translationKey: "navigation.partners",
+        href: "#partners"
+    },
+    {
+        translationKey: "navigation.blog",
+        href: "/blog"
+    },
+    {
+        translationKey: "navigation.contacts",
+        href: "#contacts"
+    }
+];
+
+module.exports = {
+    leftMenu,
+    rightMenu
+};
