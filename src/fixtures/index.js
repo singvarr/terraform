a400eb6cf46de4c7001575d73bdbb32d0fe360a0
sihorjs <sihorjs@gmail.com>
2019-10-13 21:40:07 +0300

    add main file in fixtures

---
+++ src/fixtures/index.js
@@ -0,0 +1,17 @@
+const createFixture = require("./createFixture");
+const getRandomPartner = require("./getRandomPartner");
+const getRandomReview = require("./getRandomReview");
+const getRandomWorker = require("./getRandomWorker");
+const menu = require("./menu");
+const services = require("./services");
+const stats = require("./stats");
+
+module.exports = {
+    createFixture,
+    getRandomPartner,
+    getRandomReview,
+    getRandomWorker,
+    menu,
+    services,
+    stats
+};
