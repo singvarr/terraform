79386b72f1453c25bd23c37d1d4f8ee04de154ee
sihorjs <sihorjs@gmail.com>
2019-10-14 12:14:21 +0300

    add main file of services fixture

---
+++ src/fixtures/services/index.js
@@ -0,0 +1,20 @@
+const services = [
+    {
+        name: "./sofa.png",
+        src: "3D визуализация интерьера"
+    },
+    {
+        name: "./house.png",
+        src: "3D визуализация экстерьера"
+    },
+    {
+        name: "./bear.png",
+        src: "3D моделирование"
+    },
+    {
+        name: "./zombie.png",
+        src: "Интерактивная визуализация"
+    }
+];
+
+module.exports = services;
