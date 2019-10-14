5ba64a6fff7c1d7fc156abc994764fc71db9981c
sihorjs <sihorjs@gmail.com>
2019-10-14 13:09:02 +0300

    add i18n config and register helper for translation

--- server.js
+++ server.js
@@ -1,17 +1,44 @@
 const path = require("path");
 const express = require("express");
 const exphbs = require("express-handlebars");
+const i18n = require("i18n");
+const faker = require("faker");
 require("dotenv").config();
 
-const { SRC_DIRECTORY } = require("./constants");
+const { DIST_DIRECTORY, SRC_DIRECTORY } = require("./constants");
+const {
+    contacts,
+    createFixture,
+    getRandomGallery,
+    getRandomPartner,
+    getRandomReview,
+    getRandomWorker,
+    menu: navigation,
+    services,
+    stats
+} = require("./src/fixtures");
 
 const { PORT } = process.env;
 
 const app = express();
 
-app.use(express.static(path.join("dist")));
+app.use(express.static(DIST_DIRECTORY));
 
-app.engine(".hbs", exphbs({ extname: ".hbs" }));
+i18n.configure({
+    defaultLocale: "ru",
+    directory: path.join(SRC_DIRECTORY, "locales")
+});
+app.use(i18n.init);
+
+const hbs = exphbs.create({
+    extname: ".hbs",
+    helpers: {
+        __: function() {
+            return i18n.__.apply(this, arguments);
+        }
+    }
+});
+app.engine(".hbs", hbs.engine);
 app.set("view engine", ".hbs");
 app.set("views", path.join(SRC_DIRECTORY, "views"));
 
