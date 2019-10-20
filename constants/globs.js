23bee0cff0c2a40ae1b613cb90d860bc8be89925
sihorjs <sihorjs@gmail.com>
2019-10-20 20:51:24 +0300

    add new glob

--- constants/globs.js
+++ constants/globs.js
@@ -1,9 +1,14 @@
-const paths = require("..");
+const paths = require("./paths");
 
 module.exports = {
-    ALL_JS: `${paths.ROOT_PATH}/**/*.js `,
+    ALL_JS: [
+        `${paths.ROOT_PATH}/**/*.js`,
+        "!node_modules/**",
+        `!${paths.DIST_DIRECTORY}`
+    ],
     ASSETS: `${paths.SRC_DIRECTORY}/assets/{fonts,icons}/**/*.*`,
     IMAGES: `${paths.SRC_DIRECTORY}/fixtures/**/*.{png,jpeg,jpg}`,
     JS: `${paths.SRC_DIRECTORY}/js/**/*.js`,
-    LESS: `${paths.SRC_DIRECTORY}/less/**/*.less`
+    LESS: `${paths.SRC_DIRECTORY}/less/**/*.less`,
+    STATIC: `${paths.SRC_DIRECTORY}/**/*.*`
 };
